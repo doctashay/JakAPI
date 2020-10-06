4821de409e68d25f14d195af8222086cc22c1a8c
doctashay <shabennett2@gmail.com>
2020-10-06 17:24:42 -0600

    Add some prints

--- JakAPI.py
+++ JakAPI.py
@@ -1,11 +1,14 @@
 import srcomapi, srcomapi.datatypes as dt
 import os
-api = srcomapi.SpeedrunCom(); api.debug = 1 
+api = srcomapi.SpeedrunCom(); api.debug = 0
 
+print("Grabbing game name")
 game = api.search(srcomapi.datatypes.Game, {"name": "jak and daxter: the precursor legacy"})[0]
 assert(game.name == "Jak and Daxter: The Precursor Legacy")
 
 print(game.name)
 
+print("Grabbing categories")
 print(game.categories)
+print("Grabbing No FCS WR")
 print(game.categories[1].records[0].runs[0]["run"].times)
